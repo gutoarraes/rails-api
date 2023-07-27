11eff9e7d84207c860e682a3fee1559632526007
Augusto Arraes <augustobarraes@gmail.com>
2023-07-26 22:10:17 -0400

    working create function

--- app/controllers/api/users_controller.rb
+++ app/controllers/api/users_controller.rb
@@ -1,11 +1,11 @@
 class Api::UsersController < ApplicationController
 
-  def show
+  def show # ok
     user = User.find(params[:id])
     render json: {status: 'Success', message: 'User found', data: user}, status: :ok
   end
 
-  def index
+  def index # ok
     users = User.all
     render json: {status: 'Success', message: 'All users', data: users}, status: :ok
   end
@@ -13,28 +13,38 @@ class Api::UsersController < ApplicationController
   def create
     user = User.create(user_params)
     if user.save
-      render json: {status: 'Success', message: 'User created', data: user}, status: :ok
+      render json: {status: 'success', message: 'User created', data: user}, status: :ok
     else
-      render json: {status: 'ERROR', message: 'User not created', data: user.errors}, status: :unprocessable_entity
+      render json: {status: 'ERROR', message: 'User not created', data: user.errors.full_messages}, status: :unprocessable_entity
     end
   end
 
   def update
-    @user = User.find(params[:id])
-    @user.update(user_params)
-    @user.save
-    render json: {status: 'Updated', message: 'user successfully updated'}, status: :ok
+    byebug
+    user = User.find(params[:id])
+    if user.update(user_params)
+      render json: {status: 'success', message: 'user successfully updated'}, status: :ok
+    else
+      render json: { message: 'failed to update user', data: user.errors.full_messages}, status: :unprocessable_entity
+    end
   end
 
-  def destroy
+  def destroy # ok
     user = User.find(params[:id])
     user.destroy
-    render json: {status: 'Deleted', message: 'user successfully deleted'}, status: :ok
+    render json: {status: 'success', message: 'ser successfully deleted'}, status: :ok
   end
 
   private
 
   def user_params
-    params.require(:password, :email).permit(:id)
+    params.require(:user).permit(:password, :email)
   end
+
+  # user.attributes{email:, password:}
+  # def email
+  #   params.require(:email)
+  # end
+  #
+
 end
